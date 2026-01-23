1d646c8c0028f190d80795a6189838ddc8c05fd4
leodovqa <leoqatester@gmail.com>
2026-01-23 17:43:02 +0200

    script update

--- start_pal_data_tool.bat
+++ start_pal_data_tool.bat
@@ -1,6 +1,12 @@
 @echo off
+title Palworld Data Tool Server
 echo Changing directory to the project folder...
 cd /d "D:\Github\palworld-data-tool"
-echo Listing available npm scripts...
-npm run
-pause
+
+:start
+echo Starting the Palworld Data Tool server...
+npm start
+
+echo Server stopped unexpectedly. Restarting in 5 seconds...
+timeout /t 5 /nobreak
+goto start
